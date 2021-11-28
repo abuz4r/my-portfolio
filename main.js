f0dcb52e51a62d2c762a1368d89b5e3b78ff0df5
abuz4r <abuzar98@outlook.com>
2021-11-28 00:16:57 +0000

    MAIN

--- main.js
+++ main.js
@@ -97,3 +97,12 @@ btn.addEventListener('click', function(){
 //   var randomQuote = movieQuotes[Math.floor(Math.random() * movieQuotes.length)]
 //   output.innerHTML = 'output: ' + randomQuote;
 // })
+
+
+
+
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
